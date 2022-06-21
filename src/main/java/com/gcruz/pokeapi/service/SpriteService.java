caab3ea63fc7fed6cfb50f9483c58004f8165d2b
Gianina Cruz <gianina.v.cruz@gmail.com>
2022-06-20 23:29:18 -0300

    Effect and Ability classes removed

--- src/main/java/com/gcruz/pokeapi/service/SpriteService.java
+++ src/main/java/com/gcruz/pokeapi/service/SpriteService.java
@@ -6,13 +6,13 @@ import com.gcruz.pokeapi.exception.NotFoundException;
 import java.util.List;
 
 public interface SpriteService {
-    Sprite create(Sprite pokemon) throws Exception;
+    Sprite create(Sprite sprite) throws Exception;
 
     List<Sprite> findAll() throws Exception;
 
     Sprite findById(long id) throws NotFoundException;
 
-    Sprite update(Sprite pokemon) throws Exception;
+    Sprite update(Sprite sprite) throws Exception;
 
     void delete(long id) throws Exception;
 }
