9bd4f26a1cd1e1ad847512ef7ba9a2d9177d7bf8
Gianina Cruz <gianina.v.cruz@gmail.com>
2022-06-23 18:42:23 -0300

    DamageRelations deleted

--- src/main/java/com/gcruz/pokeapi/repository/DamageRelationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gcruz.pokeapi.repository;
-
-import com.gcruz.pokeapi.entity.DamageRelation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DamageRelationRepository extends JpaRepository<DamageRelation, Long> {
-}
