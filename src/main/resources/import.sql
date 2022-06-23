3f02325e7299357b5b64b837df5323265462c9f5
Gianina Cruz <gianina.v.cruz@gmail.com>
2022-06-23 15:02:08 -0300

    Fixing sql queries to avoid duplicated key issues

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,36 +1,37 @@
-INSERT INTO stats (stats_id, health_points, attack, defense, speed_attack, speed_defense, speed, total) VALUES (1, 49, 49, 45, 65, 65, 45, 318);
-INSERT INTO stats (stats_id, health_points, attack, defense, speed_attack, speed_defense, speed, total) VALUES (2, 60, 62, 63, 80, 80, 60, 405);
-INSERT INTO stats (stats_id, health_points, attack, defense, speed_attack, speed_defense, speed, total) VALUES (3, 80, 82, 83, 100, 100, 80, 525);
+INSERT INTO stats (health_points, attack, defense, speed_attack, speed_defense, speed, total) VALUES (49, 49, 45, 65, 65, 45, 318);
+INSERT INTO stats (health_points, attack, defense, speed_attack, speed_defense, speed, total) VALUES (60, 62, 63, 80, 80, 60, 405);
+INSERT INTO stats (health_points, attack, defense, speed_attack, speed_defense, speed, total) VALUES (80, 82, 83, 100, 100, 80, 525);
 
-INSERT INTO regions (region_id, name) VALUES (1, 'Kanto');
-INSERT INTO regions (region_id, name) VALUES (2, 'Johto');
+INSERT INTO regions (name) VALUES ('Kanto');
+INSERT INTO regions (name) VALUES ('Johto');
 
-INSERT INTO generations (generation_id, name, region_id) VALUES (1, 'Generation I', 1);
-INSERT INTO generations (generation_id, name, region_id) VALUES (2, 'Generation II', 2);
+INSERT INTO generations (name, region_id) VALUES ('Generation I', 1);
+INSERT INTO generations (name, region_id) VALUES ('Generation II', 2);
 
-INSERT INTO artworks (artwork_id, url) VALUES (1, 'https://img.pokemondb.net/artwork/bulbasaur.jpg');
-INSERT INTO artworks (artwork_id, url) VALUES (2, 'https://img.pokemondb.net/artwork/ivysaur.jpg');
-INSERT INTO artworks (artwork_id, url) VALUES (3, 'https://img.pokemondb.net/artwork/venusaur.jpg');
+INSERT INTO artworks (url) VALUES ('https://img.pokemondb.net/artwork/bulbasaur.jpg');
+INSERT INTO artworks (url) VALUES ('https://img.pokemondb.net/artwork/ivysaur.jpg');
+INSERT INTO artworks (url) VALUES ('https://img.pokemondb.net/artwork/venusaur.jpg');
 
-INSERT INTO types (type_id, name) VALUES (1, 'Bug');
-INSERT INTO types (type_id, name) VALUES (2, 'Dragon');
-INSERT INTO types (type_id, name) VALUES (3, 'Electric');
-INSERT INTO types (type_id, name) VALUES (4, 'Fighting');
-INSERT INTO types (type_id, name) VALUES (5, 'Fire');
-INSERT INTO types (type_id, name) VALUES (6, 'Flying');
-INSERT INTO types (type_id, name) VALUES (7, 'Ghost');
-INSERT INTO types (type_id, name) VALUES (8, 'Grass');
-INSERT INTO types (type_id, name) VALUES (9, 'Ground');
-INSERT INTO types (type_id, name) VALUES (10, 'Ice');
-INSERT INTO types (type_id, name) VALUES (11, 'Normal');
-INSERT INTO types (type_id, name) VALUES (12, 'Poison');
-INSERT INTO types (type_id, name) VALUES (13, 'Psychic');
-INSERT INTO types (type_id, name) VALUES (14, 'Rock');
-INSERT INTO types (type_id, name) VALUES (15, 'Water');
 
-INSERT INTO pokemons (pokemon_id, name, height, weight, artwork_id, generation_id, region_id, stats_id) VALUES (1,'Bulbasaur', 0.7, 6.9, 1, 1, 1 ,1);
-INSERT INTO pokemons (pokemon_id, name, height, weight, artwork_id, generation_id, region_id, stats_id) VALUES (2,'Ivysaur', 1.0, 13.0, 2, 1, 1 ,2);
-INSERT INTO pokemons (pokemon_id, name, height, weight, artwork_id, generation_id, region_id, stats_id) VALUES (3,'Venusaur', 2.0, 100.0, 3, 1, 1 ,3);
+INSERT INTO types (name) VALUES ('Bug');
+INSERT INTO types (name) VALUES ('Dragon');
+INSERT INTO types (name) VALUES ('Electric');
+INSERT INTO types (name) VALUES ('Fighting');
+INSERT INTO types (name) VALUES ('Fire');
+INSERT INTO types (name) VALUES ('Flying');
+INSERT INTO types (name) VALUES ('Ghost');
+INSERT INTO types (name) VALUES ('Grass');
+INSERT INTO types (name) VALUES ('Ground');
+INSERT INTO types (name) VALUES ('Ice');
+INSERT INTO types (name) VALUES ('Normal');
+INSERT INTO types (name) VALUES ('Poison');
+INSERT INTO types (name) VALUES ('Psychic');
+INSERT INTO types (name) VALUES ('Rock');
+INSERT INTO types (name) VALUES ('Water');
+
+INSERT INTO pokemons (name, height, weight, artwork_id, generation_id, region_id, stats_id) VALUES ('Bulbasaur', 07, 69, 1, 1, 1 ,1);
+INSERT INTO pokemons (name, height, weight, artwork_id, generation_id, region_id, stats_id) VALUES ('Ivysaur', 10, 130, 2, 1, 1 ,2);
+INSERT INTO pokemons (name, height, weight, artwork_id, generation_id, region_id, stats_id) VALUES ('Venusaur', 20, 1000, 3, 1, 1 ,3);
 
 INSERT INTO pokemon_type (pokemon_id, type_id) VALUES (1, 8);
 INSERT INTO pokemon_type (pokemon_id, type_id) VALUES (1, 13);
