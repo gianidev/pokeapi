4969b390db78c405e0c4dbcc301ac3ad57650a5b
Gianina Cruz <gianina.v.cruz@gmail.com>
2022-06-23 18:42:55 -0300

    Remove damageRelation prop in Type entity

--- src/main/java/com/gcruz/pokeapi/entity/Type.java
+++ src/main/java/com/gcruz/pokeapi/entity/Type.java
@@ -1,8 +1,6 @@
 package com.gcruz.pokeapi.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.*;
 import java.util.List;
@@ -15,22 +13,11 @@ public class Type {
     @Column(name = "type_id")
     private long id;
     private String name;
-    @JsonBackReference
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "damage_relation_id")
-    private DamageRelation damageRelation;
     @JsonIgnoreProperties("types")
     @ManyToMany(mappedBy = "types")
     private List<Pokemon> pokemonList;
 
     public Type() {
-    }
-
-    public Type(long id, String name, DamageRelation damageRelation,
-                List<Pokemon> pokemonList) {
-        this.id = id;
-        this.name = name;
-        this.damageRelation = damageRelation;
     }
 
     public long getId() {
@@ -47,14 +34,6 @@ public class Type {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public DamageRelation getDamageRelation() {
-        return damageRelation;
-    }
-
-    public void setDamageRelation(DamageRelation damageRelation) {
-        this.damageRelation = damageRelation;
     }
 
     public List<Pokemon> getPokemonList() {
